eced73344f6c4e3fa8c4f941110020d212f59a8c
Bryce Whittington <brycew30@gmail.com>
2020-06-19 14:24:10 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,8 +3,9 @@
 
 def key_for_min_value(name_hash)
  smallest_value = 0
+ smallest_key = nil
  name_hash.each do |key, value|
    if smallest_value == 0
-     
+     smallest_value = value
  end
 end
